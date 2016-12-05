fedbeee82d362ef6e63671288fa95129c7aa8f5d
Daiki Kojima <sos.daiki.keion@gmail.com>
2016-12-05 19:44:57 +0900

    Access to Internet

--- front/Autocode2/app/src/main/java/com/monotas/wearthistoday/autocode/FormActivity.java
+++ front/Autocode2/app/src/main/java/com/monotas/wearthistoday/autocode/FormActivity.java
@@ -1,89 +1,154 @@
 package com.monotas.wearthistoday.autocode;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import okhttp3.Response;
+
 
 public class FormActivity extends AppCompatActivity {
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_form);
+        setContentView(R.layout.activity_main);
         Intent intent = getIntent();
         Bundle p = intent.getExtras();
-        Bitmap x = (Bitmap)p.get("Image");
-
-        final Handler handler = new Handler();
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        /*ここで非同期処理を書く*/
-                        String datajson = "{\"user\":{" +
-                                "\"name\":\"name1\","+
-                                "\"password\":\"password\","+
-                                "\"password_confirmation\":\"password\""+
-                                "}}";
-                        try{
-                            /*送信の部分*/
-                            String buffer= "";
-                            HttpURLConnection huc = null;
-                            URL url = new URL("");//ここの部分のURLを書き換えてね!!
-                            huc = (HttpURLConnection)url.openConnection();
-                            huc.setRequestMethod("POST");
-                            huc.setInstanceFollowRedirects(false);
-                            huc.setRequestProperty("Accept-Language", "jp");
-                            huc.setDoOutput(true);
-                            huc.setRequestProperty("Content-Type","application/json; charset=utf-8");
-                            OutputStream os = huc.getOutputStream();
-                            PrintStream ps = new PrintStream(os);
-                            ps.print(datajson);
-                            ps.close();
-
-                            /*送信結果を受け取る*/
-                            BufferedReader reader = new BufferedReader(new InputStreamReader(huc.getInputStream(),"UTF-8"));
-                            buffer = reader.readLine();
-                            JSONArray jsonarray = new JSONArray(buffer);
-                            for (int i = 0; i < jsonarray.length(); i++) {
-                                JSONObject jsonObject = jsonarray.getJSONObject(i);
-                                Log.d("HTTP REQ", jsonObject.getString("name"));
-                            }
-                            huc.disconnect();
-
-
-                        } catch (MalformedURLException e) {
-                            e.printStackTrace();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-            }
-        });
-
+        Bitmap x = (Bitmap) p.get("Image");
+        new SendPostRequest().execute();
 
     }
+
+    public class SendPostRequest extends AsyncTask<String, Void, String> {
+
+        protected void onPreExecute(){}
+
+        protected String doInBackground(String... arg0) {
+
+            try {
+
+                URL url = new URL("http://wearthistoday.monotas.com/api/echo"); // here is your URL path
+
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("name", "abc");
+                postDataParams.put("email", "abc@gmail.com");
+                Log.e("params",postDataParams.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(postDataParams));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode=conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    BufferedReader in=new BufferedReader(new
+                            InputStreamReader(
+                            conn.getInputStream()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    return sb.toString();
+
+                }
+                else {
+                    return new String("false : "+responseCode);
+                }
+            }
+            catch(Exception e){
+                return new String("Exception: " + e.getMessage());
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(), result,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+        }
+        return result.toString();
+    }
+
 }
