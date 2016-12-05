24162a66efbdbbed1f3c090f8d65d2ec2c2ff818
kdrl <kdrl7@naver.com>
2016-12-05 18:02:41 +0900

    update back

--- back/main.py
+++ back/main.py
@@ -1,11 +1,26 @@
-from flask import Flask
+# coding=utf-8
+from flask import Flask, abort, request
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
+# "/"に入ってくる時
 @app.route('/')
 def basic_response():
-    return "Wear This TodayサービスのAPIサーバーです。詳細はhttps://github.com/kdrl/Wear-This-Today より確認ください。"
+    return "Wear This TodayサービスのAPIサーバーです。詳細はhttps://github.com/kdrl/Wear-This-Today より確認ください。\n"
+
+# "/api/echo"に入ってくる時
+# methodsにPOSTを指定すると、POSTリクエストを受けられる
+@app.route('/api/echo', methods=['POST'])
+def echo():
+    # もしももらったデータがjsonでなかったら、400を返す。(400 Bad Request)
+    if not request.json:
+        abort(400)
+    # もらったJSONをサーバで表示(debug用)
+    print(request.json)
+    # JSONを返す。
+    return json.dumps(request.json)
 
 if __name__ == '__main__':
-    app.run(port=80,host='0.0.0.0')
+    app.run(port=3000, host='0.0.0.0', debug=True)
