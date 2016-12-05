19c3506810b05ba46b6fd9b4f2dbb4be594f7aa5
Daiki Kojima <sos.daiki.keion@gmail.com>
2016-12-05 22:37:11 +0900

    Send Json to the server

--- front/Autocode2/app/src/main/java/com/monotas/wearthistoday/autocode/FormActivity.java
+++ front/Autocode2/app/src/main/java/com/monotas/wearthistoday/autocode/FormActivity.java
@@ -45,7 +45,6 @@ import okhttp3.Response;
 public class FormActivity extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,28 +52,109 @@ public class FormActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle p = intent.getExtras();
         Bitmap x = (Bitmap) p.get("Image");
-        final Handler handler = new Handler();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        final String json =
-                                "{\"user\":{" +
-                                        "\"name\":\"name1\","+
-                                        "\"password\":\"password\","+
-                                        "\"password_confirmation\":\"password\""+
-                                        "}}";
-                        String buffer = "";
-                        HttpURLConnection huc = null;
-                        URL url = new URL("")
-                    }
-                });
-            }
-        }).start();
+        new SendPostRequest().execute();
+
     }
 
+    public class SendPostRequest extends AsyncTask<String, Void, String> {
 
+        protected void onPreExecute(){}
 
+        protected String doInBackground(String... arg0) {
+
+            try {
+
+                URL url = new URL("http://wearthistoday.monotas.com/api/echo"); // here is your URL path
+
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("name", "abc");
+                postDataParams.put("email", "abc@gmail.com");
+                Log.e("params",postDataParams.toString());
+
+                final String json =
+                        "{\"user\":{" +
+                                "\"name\":\"name1\","+
+                                "\"password\":\"password\","+
+                                "\"password_confirmation\":\"password\""+
+                                "}}";
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(json);
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode=conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    BufferedReader in=new BufferedReader(new
+                            InputStreamReader(
+                            conn.getInputStream()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    return sb.toString();
+
+                }
+                else {
+                    return new String("false : "+responseCode);
+                }
+            }
+            catch(Exception e){
+                return new String("Exception: " + e.getMessage());
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(), result,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+        }
+        return result.toString();
+    }
 }
