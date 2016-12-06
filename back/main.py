dfd29d732ce98a6b915fdff379e1fbcb64e67eb8
yasu <yasu@mail.com>
2016-12-07 00:36:00 +0900

    jsonを受け取って値を返せるように更新されたものをDL

--- back/main.py
+++ back/main.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from flask import Flask, abort, request
 import json
+# 自作api.pyをロード
+import api
 
 app = Flask(__name__)
 
@@ -9,10 +11,8 @@ app = Flask(__name__)
 def basic_response():
     return "Wear This TodayサービスのAPIサーバーです。詳細はhttps://github.com/kdrl/Wear-This-Today より確認ください。\n"
 
-"""
-"/api/echo"に入ってくる時
-methodsにPOSTを指定すると、POSTリクエストを受けられる
-"""
+
+# "/api/echo"にPOSTでrequestが来たら以下が発動
 @app.route('/api/echo', methods=['POST'])
 def echo():
     # もしももらったデータがjsonでなかったら、400を返す。(400 Bad Request)
@@ -22,6 +22,17 @@ def echo():
     print(request.json)
     # JSONを返す。
     return json.dumps(request.json)
+
+@app.route('/api/helloworld', methods=['POST'])
+def helloworld():
+    # もしももらったデータがjsonでなかったり、numをキーとする値を持ってなかったら、400を返す。(400 Bad Request)
+    if not request.json["num"]:
+        abort(400)
+    # もらったJSONをサーバで表示(debug用)
+    print(request.json)
+    # api.helloworldを用いて処理した値を返す
+    result = api.helloworld(request.json["num"])
+    return result
 
 if __name__ == '__main__':
     # run application with debug mode
