3794d9b18d80d685d715176d9c421e188d7bb12d
daikikojima <daikikojima0531@icloud.com>
2016-12-09 18:15:15 +0900

    Add Login or Logout function

--- front/Autocode2/app/src/main/java/com/monotas/wearthistoday/autocode/MainActivity.java
+++ front/Autocode2/app/src/main/java/com/monotas/wearthistoday/autocode/MainActivity.java
@@ -2,23 +2,59 @@ package com.monotas.wearthistoday.autocode;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.util.Arrays;
 
 import io.realm.Realm;
 
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_CAPTURE_IMAGE = 100;
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+    private AccessToken accessToken;
+    SharedPreferences prefs;
+
+    Button button;
+    String data;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        prefs = getSharedPreferences("token",MODE_PRIVATE);
+        data = prefs.getString("token","null");
+        if(data == "null"){
+            Log.d("MainActivity","null");
+            LogInActivity();
+        }
+        else{
+            Log.d("MainActivity","not");
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -26,16 +62,80 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void register(View v){
-        Intent intent = new Intent();
-        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        startActivityForResult(intent, REQUEST_CAPTURE_IMAGE);
+            Intent intent = new Intent();
+            intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            startActivityForResult(intent, REQUEST_CAPTURE_IMAGE);
+
+
+
     }
 
     public void closet(View v){
-        Intent intent = new Intent(this,ListActivity.class);
-        startActivity(intent);
+            Intent intent = new Intent(this,ListActivity.class);
+            startActivity(intent);
 
+
+
+    }
+
+    public void logOut(View v){
+
+            prefs.edit().clear().commit();
+            data = "null";
+
+            new GraphRequest(AccessToken.getCurrentAccessToken(), "/me/permissions/", null, HttpMethod.DELETE, new GraphRequest
+                    .Callback() {
+                @Override
+                public void onCompleted(GraphResponse graphResponse) {
+
+                    LoginManager.getInstance().logOut();
+                    FacebookSdk.sdkInitialize(getApplicationContext());
+
+                    LogInActivity();
+                }
+            }).executeAsync();
+
+
+
+    }
+    public void LogInActivity(){
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        String accessToken= loginResult.getAccessToken().getToken();
+                        Log.d("Token",accessToken);
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putString("token",accessToken);
+                        editor.apply();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
+                // Set the access token using
+                // currentAccessToken when it's loaded or set.
+                //Log.d("hogehoge",currentAccessToken.toString());
+            }
+        };
     }
 
     @Override
@@ -45,6 +145,9 @@ public class MainActivity extends AppCompatActivity {
             Intent formIntent = new Intent(this,FormActivity.class);
             formIntent.putExtra("Image",capturedImage);
             startActivity(formIntent);
+        }
+        else{
+            callbackManager.onActivityResult(requestCode, resultCode, data);
         }
     }
 }
