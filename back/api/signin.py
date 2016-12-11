9c19ce0fb0fdd02c64cebf18dffb2a9315eb442f
kdrl <kdrl7@naver.com>
2016-12-11 18:22:49 +0900

    update comment

--- back/api/signin.py
+++ back/api/signin.py
@@ -1,43 +1,59 @@
+# -*- coding: utf-8 -*-
+'''
+    api/signin.py
+    ここにログイン・会員登録を行う処理を書く
+'''
 from flask import Blueprint, Response, request, abort
 import json
 import requests
 from db.extension import mysql, redis
-
+'''
+    signinというBlueprintを生成
+    これをmain.pyで読み込んでapiとして登録
+'''
 signin = Blueprint('signin', __name__)
-
+# /api/signin にPOSTがきたら以下が動作する
 @signin.route('/signin', methods=['POST'])
 def this_signin():
     res = {
         "message" : ""
     }
+    # まず、tokenをとる
     token = str(request.json["token"])
     if not token:
+        # tokenがないっということはClient側で正しくtokenを渡していないこと
         res["message"] = "you need token"
         return Response(json.dumps(res), status=400, mimetype='application/json')
-
+    # もらったtokenが正しいtokenかをfacebookを介して確認する
     params = {
         "access_token": token
     }
     url = 'https://graph.facebook.com/me'
+    # facebookから返ってくるjson形式の回答をresponse_facebookに保存
     response_facebook = requests.get(url, params=params)
     response_facebook = json.loads(response_facebook.text)
+    # 正しかったらJSONは{"id" : user_id, "name" : user_name}のような形で返ってくる
     user_id = response_facebook["id"]
     user_name = response_facebook["name"]
     if not user_id:
+        # ここで正しくuser_idが取れてないのはClientで送ってくれたtokenがおかしかったということ
         res["message"] = "your token is wrong"
         return Response(json.dumps(res), status=400, mimetype='application/json')
+    # tokenが正しかったのでこれをredisに上書きする
     redis.set(token, user_id)
-
+    # 以下では取ってきたuser_idを用いてデータが以前までになかったら新たにidとnameをMySQLのwearthistodayいうDBのuserテーブルに登録する
     connection = mysql.connect()
     cursor = connection.cursor()
     query = 'select id from user where id='+ user_id
     cursor.execute(query)
     result = cursor.fetchall()
     if not result:
+        # ユーザ新規登録
         query = 'insert into user (id, name) values ("' + user_id + '", "' + user_name + '")'
         cursor.execute(query)
         connection.commit()
         res["message"] += "your are a new user.  sign up success. "
-
+    # 回答するJSONをまとめて
     res["message"] += "login success."
+    # 以下のようにResponseを用いて返す
     return Response(json.dumps(res), status=200, mimetype='application/json')
--- back/main.py
+++ back/main.py
@@ -1,41 +1,54 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+'''
+    main.py
+    Back-end applicationの中心
+    python main.py より起動
+    前提条件
+    1. MySQLが起動できていること
+        1.1 dump.sqlでDBの骨子を保存しといた
+        1.2 dump.sqlを適応する方法はREADME.mdを参照
+    2. Redisが起動できていること
+'''
 from flask import Flask
 '''
     db setting
     以下のようにDBを読み込むことが可能
+    Redisもモナ軸読み込むことが可能であるが、このmain.pyではまだ、必要なかったので読んでない
 '''
 from db.extension import mysql
 '''
-    register api
-    変数の名前が重なることで正しく読み込まれない問題があった
-    as を用いて回避する
+    APIを読み込む
+    変数の名前が重なることで正しく読み込まれない問題があったので as を用いて別名をつけることで解決
 '''
 from api.test import test as testApi
 from api.signin import signin as signinApi
-
+'''
+    Flaskアプリケーション
+'''
 app = Flask(__name__)
 '''
     MySQL configurations
+    DB接続設定
 '''
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'kdrl'
 app.config['MYSQL_DATABASE_DB'] = 'wearthistoday'
 mysql.init_app(app)
-
 '''
     register blueprint
     APIを登録する
 '''
 app.register_blueprint(testApi, url_prefix='/api/test')
 app.register_blueprint(signinApi, url_prefix='/api')
-
 '''
     "/"に入ってくる時
 '''
 @app.route('/')
 def basic_response():
     return "Wear This TodayサービスのAPIサーバーです。詳細はhttps://github.com/kdrl/Wear-This-Today より確認ください。\n"
-
+'''
+    サーバー起動
+'''
 if __name__ == '__main__':
     # run application with debug mode
     app.run(port=3000, host='0.0.0.0', debug=True)
