163cccf6ae97ff96eb2ffcd2b906bc74e9db1640
daikikojima <daikikojima0531@icloud.com>
2016-12-12 11:58:08 +0900

    Correct the id

--- front/Autocode2/app/src/main/java/com/monotas/wearthistoday/autocode/FormActivity.java
+++ front/Autocode2/app/src/main/java/com/monotas/wearthistoday/autocode/FormActivity.java
@@ -65,7 +65,7 @@ public class FormActivity extends AppCompatActivity {
     Bitmap clothesImage;
     Realm realm;
 
-
+    SharedPreferences prefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -100,11 +100,12 @@ public class FormActivity extends AppCompatActivity {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         clothesImage.compress(Bitmap.CompressFormat.PNG,100,baos);
         final byte[] mImageData = baos.toByteArray();
-
+        prefs = getSharedPreferences("size",MODE_PRIVATE);
+        int data = prefs.getInt("size",0);
         /*データの登録(ローカル)*/
-        RealmResults<ClothesData> query =realm.where(ClothesData.class).findAll();
         realm.beginTransaction();
-        int id = query.size() + 1;
+        int id = data + 1;
+        prefs.edit().putInt("size",id).apply();
         ClothesData clothesData = realm.createObject(ClothesData.class);
         clothesData.setColorText(colorText);
         clothesData.setTypeText(typeText);
@@ -120,7 +121,7 @@ public class FormActivity extends AppCompatActivity {
             obj.put("type",typeText);
             obj.put("Image",mImageData);
             JSONObject alldata = new JSONObject();
-            SharedPreferences prefs = getSharedPreferences("token",MODE_PRIVATE);
+            prefs = getSharedPreferences("token",MODE_PRIVATE);
             String dataString = prefs.getString("token","null");
             String url = "http://wearthistoday.monotas.com/api/test/echo";
             alldata.put("token",dataString);
