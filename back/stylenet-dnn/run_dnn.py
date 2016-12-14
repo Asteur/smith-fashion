5d8b9c6b8963d88f24dffb862ed848d2b5439a83
KDRL <KDRL@KDRLui-iMac.local>
2016-12-15 03:41:42 +0900

    update

--- back/stylenet-dnn/run_dnn.py
+++ back/stylenet-dnn/run_dnn.py
@@ -17,10 +17,10 @@ class MNIST_Chain(Chain):
     #パラメータを含む関数の宣言 主に結合について 
     def __init__(self): 
         super(MNIST_Chain, self).__init__( 
-            l1=L.Linear(128,1000), 
-            l2=L.Linear(1000,1000), 
-            l3=L.Linear(1000,1000), 
-            l4=L.Linear(1000,2), 
+            l1=L.Linear(128,128), 
+            l2=L.Linear(128,128), 
+            l3=L.Linear(128,128), 
+            l4=L.Linear(128,2), 
         ) 
     #損失関数(交差エントロピー誤差関数)の定義 
     def __call__(self,x,y): 
@@ -43,11 +43,11 @@ optimizer.setup(model)
 
 """Learn and Test""" 
 #ミニバッチ法を用いる 
-max_epoch = 3 #回繰り返す 
+max_epoch = 5 #回繰り返す 
 #データサイズ 
-n = 450
+n = 1000
 #バッチサイズ 
-bs = 150 
+bs = 200 
 training_error=np.zeros(max_epoch) 
 test_error=np.zeros(max_epoch) 
 for epoch in range(max_epoch): 
